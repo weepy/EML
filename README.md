12c7f407984aaa0b5058264c733294646887c997
weepy <jonahfox@gmail.com>
2013-06-04 09:54:39 +0100

    first

--- README.md
+++ README.md
@@ -2,3 +2,37 @@ EML
 ===
 
 EaselJS Markup Language
+
+
+Show me the code
+----------------
+
+  <stage id="stage" width="960" height="400">
+    <circle id="c1" x=10 y=10 fill=green></circle>
+    <circle id="c2" x=50 y=50 radius=10 fill=black></circle>
+    <text x=100 y=100 color=red scaleX=2>hello</text>
+  </stage>
+
+
+  <script>
+    var stage = EML( '#stage' )
+    stage.update()
+  </script>
+
+
+![Screeny](http://d.pr/i/UhAE "Screeny")
+
+
+Example
+-------
+
+Open example.html
+
+Future
+------
+
+* use stage.$ 
+  * as a proxy to the DOM that's shadowing the easeljs
+  * or build a simple index
+
+* remove jquery dependency
---
+++ eml.js
@@ -0,0 +1,99 @@
+function EML(x, parent) {
+  var node = $(x)[0]
+  var attr = {}  
+  var a = node.attributes
+  for(var i=0; i<a.length; i++) {
+    var name = a[i].name
+
+    // hack to rename scalex to scaleX
+    if(name.length > 1)
+      name = name.replace(/x$/,'X').replace(/y$/,'Y')
+    attr[name] = a[i].value
+  }
+    
+
+  attr.node = node
+  attr.innerHTML = node.innerHTML
+
+  var tag = node.tagName.toLowerCase()
+  var klass = EML.classes[tag]
+
+  if(!klass) console.log("no such class " + tag)
+
+  var obj = klass(attr)
+  
+  if(parent)
+    parent.addChild(obj)
+
+  $(node)
+    .children().each(function() {
+      EML(this, obj)
+    })
+
+  return obj
+}
+
+EML.object = function(base, x) {
+
+  function object(o) {
+    if(!(this instanceof object)) return new object(o)
+    o && this.extend(o)
+
+    base && base.prototype.initialize.call(this)
+    this.initialize && this.initialize(o)
+  }
+
+  if(base) {
+    function ctor() {}
+    ctor.prototype = base.prototype
+    object.prototype = new ctor()
+    object.prototype.constructor = base;
+  }
+
+  for(var key in x)
+    object.prototype[key] = x[key]
+
+  
+  object.prototype.extend = function (o) {
+    for(var key in o)
+      this[key] = o[key]
+    return this
+  }
+
+  return object
+}
+
+EML.classes = {
+
+  stage: function(o) {
+    var canvas = $('<canvas>')
+    canvas.insertAfter(o.node)
+    o.node.style.display = 'none'
+    delete o.node
+    canvas.attr( o )
+
+    var stage = new createjs.Stage(canvas[0])
+    return stage
+  },
+
+  text: EML.object(createjs.Container, {
+    font: '20px Arial',
+    color: 'black',
+    initialize: function() {
+      this.text = new createjs.Text(this.innerHTML, this.font, this.color);
+      this.addChild(this.text)
+    }
+  }),
+
+  circle: EML.object(createjs.Shape, {
+    radius: 100,
+    x: 0, 
+    y: 0,
+    fill: 'black',
+    initialize: function() {
+      this.graphics.beginFill(this.fill).drawCircle(this.x, this.y, this.radius)
+    }
+  })
+}
+
+
